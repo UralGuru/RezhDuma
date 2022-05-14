befaba270d3cf723c7624fc92e673c688632cff1
UralGuru <gizzatullinu@gmail.com>
2022-05-15 03:40:41 +0500

    HistoryPage

--- src/pages/HistoryPage/HistoryPage.js
+++ src/pages/HistoryPage/HistoryPage.js
@@ -1,10 +1,26 @@
 import React from "react";
+import styles from "../NewsPage/NewsPage.module.css";
+import Input from "../../components/shared/Input/Input";
+import Container from "../../components/shared/Container/Container";
 
 const HistoryPage = (props) => {
     return(
-        <div>
-            History
-        </div>
+        <Container>
+            <div className={styles.outer}>
+                <div className={styles.header}>
+                    <h2>История</h2>
+                    <Input
+                        className="page_search-input"
+                        placeholder="Поиск"
+                        value={'Поиск'}
+                        onChange={'onSearchChange'}
+                    />
+                </div>
+
+                <div className={styles.inner}>
+                </div>
+            </div>
+        </Container>
     );
 }
 
