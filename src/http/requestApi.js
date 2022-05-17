b570867200b1fbc46e073cdfc72f0b73a4e74392
dclxxxvi <t.aleksandr888@mail.ru>
2022-05-17 16:51:37 +0500

    Appeals API

--- src/http/requestApi.js
+++ src/http/requestApi.js
@@ -2,21 +2,11 @@ import { $authHost, $host } from ".";
 
 
 export const createRequest = async (data, userId) => {
-  const dat = $authHost.post(`api/appeals/user/${userId}`, data);
-  return dat;
+  await $authHost.post(`api/appeals/user/${userId}`, data);
+  return;
 }
 
 export const fetchPopularRequests = async () => {
   const {data} = await $host.get('api/appeals/popular');
-  return data;
-}
-
-export const fetchNewsWithPagination = async (count, page) => {
-  const {data} = await $host.get(`api/news?count=${count}&page=${page}`);
-  return data;
-}
-
-export const fetchOneNews = async (newsId) => {
-  const {data} = await $host.get(`api/news/${newsId}`);
   return data;
 }
